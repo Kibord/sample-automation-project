cd0a146e5ce2ef488c603605ac947e30e26603e3
Kibord <sdrobinko@gmail.com>
2022-10-28 17:32:18 +0200

    AgeChecker, AgeCheckerTest, and build.gradle changes in main branch

--- src/test/java/net/absoft/mortalwombat/AgeCheckerTest.java
+++ src/test/java/net/absoft/mortalwombat/AgeCheckerTest.java
@@ -1,2 +1,19 @@
-package net.absoft.mortalwombat;public class AgeCheckerTest {
+package net.absoft.mortalwombat;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AgeCheckerTest {
+
+    @Test
+    public void testAgedUserCanPlay(){
+        AgeChecker ageChecker = new AgeChecker();
+        Assert.assertTrue(ageChecker.canPlayGame(19),"Aged user can't play the!~ game");
+    }
+
+    @Test
+    public void testThatTooYongUsersCanNotPlay(){
+        AgeChecker ageChecker = new AgeChecker();
+        Assert.assertFalse(ageChecker.canPlayGame(17),"Younger user can play the game");
+    }
 }
