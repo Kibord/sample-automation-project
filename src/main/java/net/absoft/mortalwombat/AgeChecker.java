eee1eecfbf5280f5fd7caa7ccf232fa920b3eb69
Kibord <sdrobinko@gmail.com>
2022-10-28 16:56:09 +0200

    Changes in AgeChecker

--- src/main/java/net/absoft/mortalwombat/AgeChecker.java
+++ src/main/java/net/absoft/mortalwombat/AgeChecker.java
@@ -1,2 +1,9 @@
-package net.absoft.mortalwombat;public class AgeChecker {
+package net.absoft.mortalwombat;
+
+public class AgeChecker {
+
+    private final int MIN_AGE = 21;
+    public boolean canPlayGame(int age){
+        return age >= MIN_AGE;
+    }
 }
